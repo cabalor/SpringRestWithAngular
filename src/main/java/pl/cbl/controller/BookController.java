e6fa07544763ca702d0b5148113954f83823607d
cabalor <cabalor@gmail.com>
2017-09-19 19:20:00 +0200

    try

--- src/main/java/pl/cbl/controller/BookController.java
+++ src/main/java/pl/cbl/controller/BookController.java
@@ -4,6 +4,8 @@ package pl.cbl.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class BookController {
 		
 	}
 	
-	@RequestMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Book> books(){
 		return bk.getBooks();
 	}
